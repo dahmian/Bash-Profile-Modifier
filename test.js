bbcb0cdc250f0cb031f3ee5e50fffd0b0d262011
Dahmian Owen <dahmian.owen@mellmo.com>
2013-03-06 11:37:20 -0800

    updating test

--- test.js
+++ test.js
@@ -1,1 +1,7 @@
-require("./install.js").installAlias('alias test="hi"');
+require("./install.js").installAlias(alias());
+
+function alias() {
+  var path = require("path");
+  var rmovePath = path.resolve(__dirname, "./install.js");
+  return 'alias testInstaller="node ' + rmovePath + '"';
+}
